53d203f9f83f5ee41ab84a584dc9747a3bdcd73c
Chase Struse <chasestruse@gmail.ocm>
2023-01-04 20:19:56 -0600

    added file loader

--- src/runner.py
+++ src/runner.py
@@ -1,10 +1,20 @@
-import booltests
+import importlib.machinery
+import sys
+import types
 from inspect import getmembers, isfunction
 
-tests = [m for m in getmembers(booltests) if isfunction(m[1]) and m[0].startswith('test_')]
+def load_tests(mod):
+    return [m for m in getmembers(mod) if isfunction(m[1]) and m[0].startswith('test_')]
 
+def load_module(file):
+    loader = importlib.machinery.SourceFileLoader('testmod', file)
+    mod = types.ModuleType('testmod')
+    loader.exec_module(mod)
+    return mod
 
-def run():
+def run(file):
+    mod = load_module(file)
+    tests = load_tests(mod)
     success = True
     for test in tests:
         (test_name, test_function) = test
@@ -22,4 +32,4 @@ def run():
 
 
 if __name__ == '__main__':
-    run()
+    run(sys.argv[1])
